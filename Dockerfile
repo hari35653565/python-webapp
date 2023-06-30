e2570fc3401ce147a10958f9bee8f5cb8fc8b84b
root <root@ip-172-31-84-109.ec2.internal>
2023-06-30 08:47:01 +0000

    rechecing and edited the code

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN pip install --upgrade pip && pip install -r requirements.txt
 
 COPY . .
 
-EXPOSE 8080
+EXPOSE 8501
 
-CMD ["streamlit", "run", "app.py"]
+CMD ["streamlit", "run", "app.py", "--server.port", "8501"]
 
--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
+def calculate_terminal_value(initial, growth, years):
+    terminal_value = initial
+    for year in range(years):
+        terminal_value += growth * terminal_value
+    return terminal_value
+
 st.title("Hello Streamlit")
 st.header("Calculate % Growth")
-initial = st.number_input("Initial investment in USD")
-yr = st.number_input("Growth Period in years")
-growth = st.number_input("Growth Rate in %")
-terminal_value = 0
-current_val = initial
-for year in range(int(yr)):
-   current_val += growth * current_val
-   terminal_value = current_val
 
-# perform cashflow projections for the next 5 years
-st.write(f'Terminal value of {initial} after {yr} years at a growth rate of {growth} is {terminal_value}')
+initial = st.number_input("Initial investment in USD")
+growth = st.number_input("Growth Rate in %")
+years = st.number_input("Growth Period in years")
+
+terminal_value = calculate_terminal_value(initial, growth, int(years))
+
+st.write(f'Terminal value of {initial} after {years} years at a growth rate of {growth}% is {terminal_value}')
+
