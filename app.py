52c2412133f9d3e8a73b8e5bbeddedbbd6a8c0ce
hari35653565 <137885952+hari35653565@users.noreply.github.com>
2023-06-30 15:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 
-def calculate_terminal_value(initial, growth, years):
-    terminal_value = initial
-    for year in range(years):
-        terminal_value += growth * terminal_value
+def calculate_terminal_value(initial, years, growth_rate):
+    terminal_value = initial * (1 + growth_rate) ** years
     return terminal_value
 
 st.title("Hello Streamlit")
-st.header("Calculate % Growth")
+st.header("Calculate Terminal Value")
 
-initial = st.number_input("Initial investment in USD")
-growth = st.number_input("Growth Rate in %")
-years = st.number_input("Growth Period in years")
+initial_investment = st.number_input("Initial investment in USD")
+investment_years = st.number_input("Investment period in years")
+growth_rate = st.number_input("Growth rate in %")
 
-terminal_value = calculate_terminal_value(initial, growth, int(years))
+terminal_value = calculate_terminal_value(initial_investment, investment_years, growth_rate / 100)
 
-st.write(f'Terminal value of {initial} after {years} years at a growth rate of {growth}% is {terminal_value}')
-
+st.write(f"Terminal value of {initial_investment} after {investment_years} years at a growth rate of {growth_rate}% is {terminal_value:.2f}")
